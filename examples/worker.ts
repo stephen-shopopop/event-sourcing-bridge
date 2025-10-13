bba65da00d84205655fd1f521246974446cf5d54
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2025-10-13 12:48:50 +0200

    chore: update example to prepare sqlite queue

--- examples/worker.ts
+++ examples/worker.ts
@@ -16,11 +16,26 @@ db.exec(`
     PRAGMA optimize;
     PRAGMA busy_timeout = 5000;
 
-    CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);
+    CREATE TABLE IF NOT EXISTS users (
+      id text primary key default ('m_' || lower(hex(randomblob(16)))),
+      created text not null default (strftime('%Y-%m-%dT%H:%M:%fZ')),
+      updated text not null default (strftime('%Y-%m-%dT%H:%M:%fZ')),
+      name text not null,
+      body text not null,
+      timeout text not null default (strftime('%Y-%m-%dT%H:%M:%fZ')),
+      received integer not null default 0,
+      priority integer not null default 0
+    ) strict;
+
+    create trigger users_updated_timestamp after update on users begin
+      update users set updated = strftime('%Y-%m-%dT%H:%M:%fZ') where id = old.id;
+    end;
+
+    create index users_queue_priority_created_idx on users (name, priority desc, created);
 `);
 
 for (let i = 0; i < 1000; i++) {
-  db.exec("INSERT INTO users (name) VALUES ('john')");
+  db.exec(`INSERT INTO users (name, body) VALUES ('john', 'hello')`);
 }
 
 // Helper function that simulates a fetch operation
@@ -36,9 +51,18 @@ const fetch = async ({ signal }: { signal: AbortSignal }) => {
     signal,
     stream.Readable.from(
       db
-        .prepare(
-          "UPDATE users SET name = 'albert' WHERE id IN (SELECT id FROM users WHERE name = ? LIMIT 50) returning id, name"
-        )
+        .prepare(`
+          UPDATE users
+            SET
+                timeout = strftime('%Y-%m-%dT%H:%M:%fZ', 'now', '+1 minute'),
+                received = received + 1
+            WHERE id IN (
+              SELECT id FROM users
+              WHERE name = ? and received < 1 and strftime('%Y-%m-%dT%H:%M:%fZ') >= timeout
+              order by priority desc, created
+              LIMIT 50
+            ) returning id, name, body, created, updated, timeout, received, priority
+        `)
         .iterate('john')
     )
   );
@@ -55,7 +79,7 @@ const fetch = async ({ signal }: { signal: AbortSignal }) => {
     console.log('readable:', item);
 
     // Delete job
-    db.exec(`DELETE FROM users WHERE id = ${item.id}`);
+    db.prepare('DELETE FROM users WHERE name= ? and id = ?').run(item.name, item.id);
   }
 
   // Log the current user count
